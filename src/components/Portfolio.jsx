af4ab5c5b8e196d52069067ebede0c00ce8a3b07
Uttam Kumar <uttammaurya377@gmail.com>
2025-05-25 21:13:33 +0530

    d

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -35,7 +35,7 @@ function PortFolio() {
       id: 5,
       logo: JavaScript,
       name: "Java Projects",
-      description: "Core Java-based applications, demonstrating OOPs and data structures.",
+      // description: "Core Java-based applications, demonstrating OOPs and data structures.",
     },
   ];
 
