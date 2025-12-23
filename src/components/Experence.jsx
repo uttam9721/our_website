e502ff419f8ae696c12761371f4df404bfb56be0
Uttam Kumar <159714121+uttam9721@users.noreply.github.com>
2025-12-23 19:13:24 +0530

    Update Experence.jsx

--- src/components/Experence.jsx
+++ src/components/Experence.jsx
@@ -53,7 +53,7 @@ function Experiance() {
       className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-16"
     >
       <div>
-        <h1 className="text-3xl font-bold mb-5">Experiance</h1>
+        <h1 className="text-3xl font-bold mb-5">Experience</h1>
         <p className="  ">
           I've more than 6 months of experiance in below technologies.
         </p>
